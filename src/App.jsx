0c5c3fd1e75180abe773371823a12167c35301cf
salarGholami <salargholami021@gmail.com>
2024-08-12 03:22:42 +0330

    Search in Character base on query

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react";
 import CharacterDetail from "./components/CharacterDetail";
 import CharacterList from "./components/CharacterList";
-import NavBar from "./components/NavBar";
+import NavBar, {
+  Favourite,
+  Logo,
+  Search,
+  SearchResult,
+} from "./components/NavBar";
 import Loader from "./components/Loader";
 import toast, { Toaster } from "react-hot-toast";
 import axios from "axios";
@@ -9,24 +14,32 @@ import axios from "axios";
 function App() {
   const [characters, setCharacters] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     async function fetcHData() {
       try {
         setIsLoading(true);
         const { data } = await axios.get(
-          "https://rickandmortyapi.com/api/character"
+          `https://rickandmortyapi.com/api/character/?name=${query}`
         );
 
         setCharacters(data.results.slice(0, 3));
       } catch (err) {
+        setCharacters([]);
         toast.error(err.response.data.error);
       } finally {
         setIsLoading(false);
       }
     }
+
+    if (query.length < 3) {
+      setCharacters([]);
+      return;
+    }
+
     fetcHData();
-  }, []);
+  }, [query]);
 
   return (
     <div className="">
@@ -34,7 +47,16 @@ function App() {
       <div className="container mx-auto">
         <div className="grid grid-cols-12">
           <div className="col-span-12">
-            <NavBar numOfResult={characters.length} />
+            <NavBar>
+              <Logo />
+              <Search query={query} setQuery={setQuery} />
+              <div className="hidden md:flex">
+                <SearchResult numOfResult={characters.length} />
+              </div>
+              <div className="hidden md:flex">
+                <Favourite />
+              </div>
+            </NavBar>
           </div>
           <div className="col-span-12 md:col-span-5">
             {isLoading ? <Loader /> : <CharacterList characters={characters} />}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,33 +1,46 @@
 import { HeartIcon } from "@heroicons/react/24/outline";
 
-function NavBar({ numOfResult }) {
+function NavBar({ children }) {
   return (
     <div className="flex justify-between items-center bg-gray-700 rounded-xl my-3 mx-2 p-2">
-      <div className="text-sm md:text-base">
-        <p>
-          LOGO <span>😍</span>
-        </p>
-      </div>
-      <div className="flex justify-center items-center">
-        <input
-          type="text"
-          className="bg-gray-500 flex justify-center items-center p-3 rounded-lg "
-          placeholder="search ..."
-        />
-      </div>
-      <div className="hidden md:flex">
-        <p>Found {numOfResult} Results</p>
-      </div>
-      <div className=" hidden md:flex">
-        <button className="relative">
-          <HeartIcon className="w-6 h-6 mr-2 text-red-600" />
-          <span className="absolute bottom-3 right-0 w-2 h-2 p-2 rounded-full bg-red-600 flex justify-center items-center text-xs">
-            4
-          </span>
-        </button>
-      </div>
+      {children}
     </div>
   );
 }
 
 export default NavBar;
+
+export function Favourite() {
+  return (
+    <button className="relative">
+      <HeartIcon className="w-6 h-6 mr-2 text-red-600" />
+      <span className="absolute bottom-3 right-0 w-2 h-2 p-2 rounded-full bg-red-600 flex justify-center items-center text-xs">
+        4
+      </span>
+    </button>
+  );
+}
+
+export function Search({ query, setQuery }) {
+  return (
+    <input
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      type="text"
+      className="bg-gray-500 flex justify-center items-center p-3 rounded-lg "
+      placeholder="search ..."
+    />
+  );
+}
+
+export function SearchResult({ numOfResult }) {
+  return <p>Found {numOfResult} Results</p>;
+}
+
+export function Logo() {
+  return (
+    <p>
+      LOGO <span>😍</span>
+    </p>
+  );
+}
