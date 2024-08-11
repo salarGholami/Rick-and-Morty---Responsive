e6c6a8d9e170a3d5748af514acdab7d61570b70f
salarGholami <salargholami021@gmail.com>
2024-08-11 19:12:26 +0330

    Error handling

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import CharacterDetail from "./components/CharacterDetail";
 import CharacterList from "./components/CharacterList";
 import NavBar from "./components/NavBar";
 import Loader from "./components/Loader";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   const [characters, setCharacters] = useState([]);
@@ -10,23 +11,30 @@ function App() {
 
   useEffect(() => {
     async function fetcHData() {
-      setIsLoading(true);
-      const res = await fetch("https://rickandmortyapi.com/api/character");
-      const data = await res.json();
-      setCharacters(data.results.slice(0, 5));
-      setIsLoading(false);
+      try {
+        setIsLoading(true);
+        const res = await fetch("https://rickandmortyapi.com/api/character");
+        if (!res.ok) throw new Error("somethiong went wrong");
+        const data = await res.json();
+        setCharacters(data.results.slice(0, 3));
+      } catch (err) {
+        toast.error(err.message);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetcHData();
   }, []);
 
   return (
     <div className="">
+      <Toaster />
       <div className="container mx-auto">
         <div className="grid grid-cols-12">
           <div className="col-span-12">
             <NavBar numOfResult={characters.length} />
           </div>
-          <div className="col-span-12 md:col-span-5 ">
+          <div className="col-span-12 md:col-span-5">
             {isLoading ? <Loader /> : <CharacterList characters={characters} />}
           </div>
           <div className="col-span-12 md:col-span-7">
