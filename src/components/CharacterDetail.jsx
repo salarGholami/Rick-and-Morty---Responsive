14680e799eaa1f6809d22e68c9cebb759294baa6
salarGholami <salargholami021@gmail.com>
2024-08-14 16:37:18 +0330

    sorted Episodes

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,7 +1,5 @@
 import { ArrowUpCircleIcon } from "@heroicons/react/24/outline";
 import axios from "axios";
-// import { episodes } from "../../data/data";
-
 import { useEffect, useState } from "react";
 import Loader from "./Loader";
 import toast from "react-hot-toast";
@@ -52,85 +50,68 @@ function CharacterDetail({ selectedId, onAddFavourite, isAddToFavourite }) {
   return (
     <div className="grid grid-cols-12 gap-y-5 md:gap-y-6 m-3 mt-7 md:mt-3">
       <div className="col-span-12">
-        <div className="flex flex-row bg-gray-800 rounded-lg overflow-hidden">
-          <div className="w-44 md:w-48">
-            <img src={character.image} className="w-full" alt="" />
-          </div>
-          <div className="">
-            <div className="flex justify-between items-center p-3">
-              <div className="flex">
-                <div className="flex flex-col justify-start">
-                  <div className="flex justify-start items-center cursor-pointer">
-                    <span>{character.gender === "Male" ? "👨" : "👩"}</span>
-                    <p>{character.name}</p>
-                  </div>
-                  <div className="flex justify-start items-center">
-                    <div className="mx-2">
-                      {character.status === "Dead" ? (
-                        <span className="bg-red-700 w-2 h-2 flex justify-center items-center rounded-full"></span>
-                      ) : (
-                        <span className="bg-green-700 w-2 h-2 flex justify-center items-center rounded-full"></span>
-                      )}
-                    </div>
-                    <div className="text-xs">
-                      {character.status} - {character.species}
-                    </div>
-                  </div>
-                  <div className="flex flex-col mx-2 my-4 md:my-6">
-                    <div className="text-sm text-gray-400">
-                      last know location :
-                    </div>
-                    <div className="text-sm">{character.location.name}</div>
-                  </div>
-                  <div className="">
-                    {isAddToFavourite ? (
-                      <p className="bg-slate-600  text-sm ">Allready added To Favourites ✅</p>
-                    ) : (
-                      <button
-                      className="bg-gray-500 p-2 text-sm rounded-xl"
-                      onClick={() => onAddFavourite(character)}
-                    >
-                      Add to Favourite
-                    </button>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <CharacterSubInfo
+          onAddFavourite={onAddFavourite}
+          isAddToFavourite={isAddToFavourite}
+          character={character}
+        />
       </div>
       <div className="col-span-12">
-        <div className="flex flex-col bg-gray-800 p-3 rounded-lg">
-          <div className="flex justify-between items-center mb-5">
-            <div className="">
-              <p className="text-gray-400 text-xl">List of Episodes :</p>
-            </div>
-            <div className="">
-              <span className="w-8 h-8 p-1 flex justify-between items-center bg-gray-700 rounded-full ">
-                <ArrowUpCircleIcon />
-              </span>
-            </div>
-          </div>
-          <div className="">
-            <ul className="space-y-2">
-              {episodes.map((item, index) => {
-                return (
-                  <li
-                    className="flex justify-between items-center text-sm text-gray-400"
-                    key={index}
+        <EpisodeList episodes={episodes} />
+      </div>
+    </div>
+  );
+}
+
+export default CharacterDetail;
+
+function CharacterSubInfo({ character, isAddToFavourite, onAddFavourite }) {
+  return (
+    <div className="flex flex-row bg-gray-800 rounded-lg overflow-hidden">
+      <div className="w-44 md:w-48">
+        <img src={character.image} className="w-full" alt="" />
+      </div>
+      <div className="">
+        <div className="flex justify-between items-center p-3">
+          <div className="flex">
+            <div className="flex flex-col justify-start">
+              <div className="flex justify-start items-center cursor-pointer">
+                <span>{character.gender === "Male" ? "👨" : "👩"}</span>
+                <p>{character.name}</p>
+              </div>
+              <div className="flex justify-start items-center">
+                <div className="mx-2">
+                  {character.status === "Dead" ? (
+                    <span className="bg-red-700 w-2 h-2 flex justify-center items-center rounded-full"></span>
+                  ) : (
+                    <span className="bg-green-700 w-2 h-2 flex justify-center items-center rounded-full"></span>
+                  )}
+                </div>
+                <div className="text-xs">
+                  {character.status} - {character.species}
+                </div>
+              </div>
+              <div className="flex flex-col mx-2 my-4 md:my-6">
+                <div className="text-sm text-gray-400">
+                  last know location :
+                </div>
+                <div className="text-sm">{character.location.name}</div>
+              </div>
+              <div className="">
+                {isAddToFavourite ? (
+                  <p className="text-slate-400 text-sm ml-2">
+                    Allready added To Favourites ✅
+                  </p>
+                ) : (
+                  <button
+                    className="bg-gray-500 p-2 text-sm rounded-xl"
+                    onClick={() => onAddFavourite(character)}
                   >
-                    <div className="cursor-pointer">
-                      {String(index + 1).padStart(2, "0")} - {item.episode} :{" "}
-                      <strong>{item.name}</strong>
-                    </div>
-                    <div className="bg-gray-600 cursor-pointer py-1 px-3 rounded-lg">
-                      {item.air_date}
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
+                    Add to Favourite
+                  </button>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -138,4 +119,62 @@ function CharacterDetail({ selectedId, onAddFavourite, isAddToFavourite }) {
   );
 }
 
-export default CharacterDetail;
+function EpisodeList({ episodes }) {
+  const [sortBy, setSortby] = useState(true);
+
+  let sortedEpisodes;
+
+  if (sortBy) {
+    sortedEpisodes = [...episodes].sort(
+      (a, b) => new Date(a.created) - new Date(b.created)
+    );
+  } else {
+    sortedEpisodes = [...episodes].sort(
+      (a, b) => new Date(b.created) - new Date(a.created)
+    );
+  }
+
+  return (
+    <div className="flex flex-col bg-gray-800 p-3 rounded-lg">
+      <div className="flex justify-between items-center mb-5">
+        <div className="">
+          <p className="text-gray-400 text-xl">List of Episodes :</p>
+        </div>
+        <div className="">
+          <button
+            className="w-8 h-8 p-1 flex justify-between items-center bg-gray-700 rounded-full"
+            onClick={() => setSortby((is) => !is)}
+          >
+            <ArrowUpCircleIcon
+              className={
+                sortBy
+                  ? "rotate-0  transition duration-500 ease-in-out"
+                  : "rotate-180  transition duration-500 ease-in-out"
+              }
+            />
+          </button>
+        </div>
+      </div>
+      <div className="">
+        <ul className="space-y-2">
+          {sortedEpisodes.map((item, index) => {
+            return (
+              <li
+                className="flex justify-between items-center text-sm text-gray-400"
+                key={index}
+              >
+                <div className="cursor-pointer">
+                  {String(index + 1).padStart(2, "0")} - {item.episode} :{" "}
+                  <strong>{item.name}</strong>
+                </div>
+                <div className="bg-gray-600 cursor-pointer py-1 px-3 rounded-lg">
+                  {item.air_date}
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
